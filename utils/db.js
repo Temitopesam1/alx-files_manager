8312ef4e946716b0e0a9b9e77a8aa6e5e21f50c6
Temitopesam1 <temitopesam5@gmail.com>
2023-05-28 04:26:16 -0700

    FIX: storage problem

--- utils/db.js
+++ utils/db.js
@@ -1,20 +1,21 @@
 import { MongoClient } from 'mongodb';
 
 
-const host = process.env.DB_HOST ||'localhost';
-const port = process.env.DB_PORT || 27017;
-const database = process.env.DB_DATABASE || "files_manager";
 
 class DBClient {
     constructor(){
-        const url =  `mongodb://${host}:${port}`;
-        this.client = new MongoClient(url);
-
-        // Use connect method to connect to the server
-        this.client.connect();
-        this.db = this.client.db(database);
-        this.userCollection = this.db.collection('users');
-        this.fileCollection = this.db.collection('files');
+	    const host = process.env.DB_HOST ||'localhost';
+	    const port = process.env.DB_PORT || 27017;
+	    const database = process.env.DB_DATABASE || "files_manager";
+		    const url = `mongodb://${host}:${port}`;
+		    this.client = new MongoClient(url);
+		    // Use connect method to connect to the server
+        		await this.client.connect();
+		    	console.log("Connected");
+        		this.db = this.client.db(database);
+        		this.userCollection = this.db.collection('users');
+        		this.fileCollection = this.db.collection('files');
+	    }
 
     }
 
