2ff3bfbe4eaa5dffb5a284df7debb3910aa11577
Temitopesam1 <temitopesam5@gmail.com>
2023-05-28 12:51:08 +0100

    FEAT: Design storage for the project

--- utils/db.js
+++ utils/db.js
@@ -1,21 +1,27 @@
 import { MongoClient } from 'mongodb';
 
 
+const host = process.env.DB_HOST ||'localhost';
+const port = process.env.DB_PORT || 27017;
+const database = process.env.DB_DATABASE || "files_manager";
+
 
 class DBClient {
     constructor(){
-	    const host = process.env.DB_HOST ||'localhost';
-	    const port = process.env.DB_PORT || 27017;
-	    const database = process.env.DB_DATABASE || "files_manager";
+        const DBObj = async () =>{
 		    const url = `mongodb://${host}:${port}`;
-		    this.client = new MongoClient(url);
+		    const client = new MongoClient(url);
+
 		    // Use connect method to connect to the server
-        		await this.client.connect();
-		    	console.log("Connected");
-        		this.db = this.client.db(database);
-        		this.userCollection = this.db.collection('users');
-        		this.fileCollection = this.db.collection('files');
-	    }
+            await client.connect();
+    	    console.log("Connected");
+            return client;
+        }
+        this.client = DBObj();
+	    
+        this.db = this.client.db(database);
+		this.userCollection = this.db.collection('users');
+        this.fileCollection = this.db.collection('files');
 
     }
 
